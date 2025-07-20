c76e8d3235ef982cd7f4966d61e6cb2322ea70a8
Frankincense <frankincensewelsey@gmail.com>
2025-07-20 19:21:30 +0300

    Added Register controller, image controller, and setup cloudinary in the flask app

--- server/app.py
+++ server/app.py
@@ -6,15 +6,19 @@ from flask_migrate import Migrate
 from dotenv import load_dotenv
 from flask_cors import CORS
 from server.config import db 
+import cloudinary
 
 load_dotenv()
 
-app = Flask()
+app = Flask(__name__)
 CORS(app, supports_credentials=True)
+
 app.config.from_prefixed_env(prefix='FLASK')
 db.init_app(app=app)
 migrate = Migrate(app=app, db=db)
 bcrypt = Bcrypt(app=app)
 jwt = JWTManager(app=app)
 api = Api(app=app)
+config = cloudinary.config(secure=True)
 
+print("****1. Set up and configure the SDK:****\nCredentials: ", config.cloud_name, config.api_key, "\n")

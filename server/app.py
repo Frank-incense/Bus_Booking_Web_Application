cc65a17737aee5a5d28acb63a13de7a62a871f5e
Frankincense <frankincensewelsey@gmail.com>
2025-07-20 09:36:24 +0300

    Setup for user model, the flask app and the config for the app

---
+++ server/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_from_directory
+from flask_bcrypt import Bcrypt
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+from flask_cors import CORS
+from server.config import db 
+
+load_dotenv()
+
+app = Flask()
+CORS(app, supports_credentials=True)
+app.config.from_prefixed_env(prefix='FLASK')
+db.init_app(app=app)
+migrate = Migrate(app=app, db=db)
+bcrypt = Bcrypt(app=app)
+jwt = JWTManager(app=app)
+api = Api(app=app)
+
---
+++ server/models/user.py
@@ -0,0 +1,42 @@
+from server.config import db
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy.orm import relationship, validates
+from sqlalchemy.ext.hybrid import hybrid_property
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    email = Column(String(), unique=True)
+    _password_hash = Column(String()) 
+    image_url = Column(String())
+    role = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    def __repr__(self):
+        return f"User: {self.id}, {self.name}."
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Cannot access this value')
+    
+    @password_hash.setter
+    def password_hash(self, value):
+        from server.app import bcrypt
+        password_hash = bcrypt.generate_password_hash(value.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        from server.app import bcrypt
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+    
+    @validates('role')
+    def validate_role(self, key, value):
+        if value is 'Driver' or value is 'Admin':
+            return value
+        
+        raise ValueError('Role can only be Driver or Admin')
+        

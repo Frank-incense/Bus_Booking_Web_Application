181d833f819aa96da6bb11452147da1f9eecde4c
Jefferson <jeff.wafula1@student.moringaschool.com>
2025-07-20 18:09:05 +0300

    after updating the first sample nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,32 @@
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-buttons">
-        <Link to="/signup" className="btn join-btn">Join</Link>
-        <Link to="/login" className="btn login-btn">Login</Link>
+    <header className="navbar">
+      <div className="navbar-left">
+        <img src="/logo.png" alt="Logo" className="logo" />
+        <nav className="nav-links">
+          <Link to="/">Home</Link>
+          <a href="#">About Us</a>
+          <a href="#">Contact Us</a>
+          <div className="dropdown">
+            <button className="dropbtn">Services ▼</button>
+            <div className="dropdown-content">
+              <a href="#">Service 1</a>
+              <a href="#">Service 2</a>
+              <a href="#">Service 3</a>
+            </div>
+          </div>
+        </nav>
       </div>
-    </nav>
+      <div className="navbar-right">
+        <Link to="/signup" className="btn join">Join</Link>
+        <Link to="/login" className="btn login">Login</Link>
+      </div>
+    </header>
   );
-}
+};
 
 export default Navbar;

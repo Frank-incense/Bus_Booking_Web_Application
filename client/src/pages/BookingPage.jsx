741198f7135f0daa496aedc70370e0f84482e1a8
Frankincense <frankincensewelsey@gmail.com>
2025-08-01 17:37:48 +0300

    corrections

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchBookings, updateBooking, deleteBooking } from '../api/bookings';
 
 const BookingPage = () => {
@@ -131,4 +131,4 @@ const BookingPage = () => {
   );
 };
 
-export default UserBookingPage;
+export default BookingPage;
